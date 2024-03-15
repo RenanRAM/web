92f2f2906ff7c4f57cdbc2213e2a3a9582f91937
Testes <renanmagico22@gmail.com>
2024-03-15 17:22:31 -0300

    Resolvido o bug de não remoção do ouvinte das funções sim() e nao()

--- js/agenda-finalizador.js
+++ js/agenda-finalizador.js
@@ -10,11 +10,16 @@ document.querySelectorAll('strong.botoes').forEach(ele=>{
 		let hora = pai.getAttribute('cliente_hora');
 		let data = pai.getAttribute('cliente_data');
 		abrirConfirma('Deseja finalizar o atendimento de '+nome+', marcado para a data '+data+' | '+hora+' ?',()=>{
+			//função em sim
 			enviar('ajaxPHP/controladorAgendaFinalizar.php',gerarFormSingle('finalizar',cliente_id),(respostaAjax)=>{
+				//função executada em caso de sucesso do ajax
 				console.log(respostaAjax);
-			},null);
+			},()=>{
+				//função em caso de erro do ajax
+			});
 		},()=>{
-
+			//função em não
+			console.log('Não confirmado');
 		});
 	});
 });
@@ -26,6 +31,12 @@ function gerarFormSingle(cod,dado){
 }
 //abrir caixa de confirmação
 function abrirConfirma(msg = '',sim = null,nao= null){
+	let controle = new AbortController();
+
+	function chamaApagaOuvinte(controlador,chamada){
+		chamada();
+		controlador.abort();
+	}
 	let fundo = document.querySelector('div.confirma-fundo');
 	fundo.attributeStyleMap.set('display','block');
 	let janela = fundo.querySelector('div.confirma-janela');
@@ -35,14 +46,14 @@ function abrirConfirma(msg = '',sim = null,nao= null){
 	let btn_sim = janela.querySelector('div.btn-confirma div.sim');
 	btn_sim.addEventListener('click',()=>{
 		if(sim instanceof Function){
-			sim();
+			chamaApagaOuvinte(controle,sim);
 		}
 		fundo.attributeStyleMap.set('display','none');
-	},{'once':true});
+	},{'once':true,'signal': controle.signal});
 	btn_nao.addEventListener('click',()=>{
 		if(nao instanceof Function){
-			nao();
+			chamaApagaOuvinte(controle,nao);
 		}
 		fundo.attributeStyleMap.set('display','none');
-	},{'once':true});
+	},{'once':true,'signal': controle.signal});
 }
